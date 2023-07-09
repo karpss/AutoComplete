86a66e4419755ac4f38f6bbaf559602daa24864b
karpss <ola.fam10@gmail.com>
2023-07-09 19:19:28 +0100

    Implemented the Products List. Work is not totally done, need to work on styling and higlighted letters

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,16 +1,35 @@
 /* eslint-disable */ 
 import React from 'react';
+import styles from './ProductsList.module.css';
 
 import { Product } from '../../product-type';
 
-const ProductsList: React.FC = ({
-    
-  }) => {
-    return (
-      <div >
-        Hello
-      </div>
-    );
-  };
+interface ProductListProps {
+  products: Product[];
+  handleProductClick: (product: Product) => void;
   
-  export default ProductsList;
+};
+
+const ProductList: React.FC<ProductListProps> = ({
+  products,
+  handleProductClick,
+  
+}) => {
+  return (
+    <div className={styles.resultProductContainer}>
+      {products.map((product) => (
+        <div
+          key={product.id}
+          
+          className={styles.resultList}
+          onClick={() => handleProductClick(product)}
+        >
+          <p className="productTitle">{(product.title)}</p>
+          <img src={product.image} alt="product-image" className={styles.productImage} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
